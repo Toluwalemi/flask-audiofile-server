55dcf8f95919cbd0c5cd9a598a9afa9e47972c62
Toluwalemi <toluwalemisrael@gmail.com>
2021-03-15 11:24:47 +0100

    feature: register cli command

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from flask.cli import FlaskGroup
 
-from src import app
+from src import app, db
 
 cli = FlaskGroup(app)
+
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     cli()
