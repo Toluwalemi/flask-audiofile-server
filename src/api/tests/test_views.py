159b65a2b45336a25ada4d291936c50836b97929
Toluwalemi <toluwalemisrael@gmail.com>
2021-03-18 10:53:30 +0100

    feature: add test for post on model song

--- src/api/tests/test_views.py
+++ src/api/tests/test_views.py
@@ -19,21 +19,34 @@ class TestAudioService(BaseTestCase):
         """Ensure a new audio file type can be added to the database"""
         with self.client:
             response = self.client.post(
-                'audio',
+                '/audio',
                 data=json.dumps({
-
+                    'audioFileType': 'song',
+                    'audioFileMetadata': {
+                        'name': 'hold_me_down',
+                        'duration': 180,
+                    }
                 }),
-                content_typ='application/json',
+                content_type='application/json',
             )
-            data = json.loads(response.data.decode())
-            self.assertEqual(response.status_code, 201)
+
+            data = json.loads((response.data.decode()))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('hold_me_down was added!', data['message'])
+            self.assertIn('success', data['status'])
+
+            print("\n=============================================================")
 
     def test_add_audio_file_invalid_json(self):
         """Ensure error is thrown if the JSON object is empty"""
+        print("\n=============================================================")
+
         pass
 
     def test_add_duplicate_audio_file(self):
         """Ensure that an error is returned for a duplicate entry"""
+        print("\n=============================================================")
+
         pass
 
 
