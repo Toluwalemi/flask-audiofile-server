629d6cca79f0034293e4a4a21e2221af377959c6
Toluwalemi <toluwalemisrael@gmail.com>
2021-03-18 15:05:06 +0100

    feature: create post request

--- src/api/views.py
+++ src/api/views.py
@@ -1,49 +1,26 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
+from flask_classful import FlaskView
 
 from src import db
 from src.api.models import Song
 
-audio_blueprint = Blueprint('audio', __name__)
 
+class AudioView(FlaskView):
+    """Class-based views for HTTP Methods: POST, GET, PUT & DELETE"""
 
-@audio_blueprint.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
-
-
-@audio_blueprint.route('/audio', methods=['POST'])
-def add_audio_type():
-    file_type = 'song'
-    new_audio = {}
-    if file_type:
+    def post(self):
+        new_audio = {}
         post_data = request.get_json()
-        new_audio['audioFileType'] = file_type
-        new_audio['audioFileMetadata'] = {'name': post_data.get('audioFileMetadata').get('name'),
-                                          'duration': post_data.get('audioFileMetadata').get('duration')
-                                          }
-        name = new_audio['audioFileMetadata'].get('name')
-        duration = new_audio['audioFileMetadata'].get('duration')
-        db.session.add(Song(name=name, duration=duration))
-        db.session.commit()
-        response_object = {
-            'status': 'success',
-            'message': f'{name} was added!'
-        }
-        return jsonify(response_object), 200
-
-# class Audio(MethodView):
-#     # route_base = '/v1'
-#
-#     def post(self):
-#         audioFileType = 'song'
-#         if audioFileType:
-#             post_data = request.get_json()
-#             audioFileMetadata = {}
-#             audioFileMetadata['name'] = post_data.get('name')
-#             audioFileMetadata['duration'] = post_data.get('duration')
-#             db.session.add(Song(name=audioFileMetadata['name'], duration=audioFileMetadata['duration']))
-#             db.session.commit()
-#             return 200
+        if post_data['audioFileType'] == 'song':
+            new_audio['audioFileMetadata'] = {'name': post_data.get('audioFileMetadata').get('name'),
+                                              'duration': post_data.get('audioFileMetadata').get('duration')
+                                              }
+            name = new_audio['audioFileMetadata'].get('name')
+            duration = new_audio['audioFileMetadata'].get('duration')
+            db.session.add(Song(name=name, duration=duration))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{name} was added!'
+            }
+            return jsonify(response_object), 200
