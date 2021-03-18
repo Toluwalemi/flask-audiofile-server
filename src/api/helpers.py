3cf226bd0b461f399fe96170456700ad0e9df58c
Toluwalemi <toluwalemisrael@gmail.com>
2021-03-18 20:48:49 +0100

    feature: add helper function to create a song

--- src/api/helpers.py
+++ src/api/helpers.py
@@ -1,8 +1,20 @@
-"""
-Helper function used in src.api.routes.main_urls.py
-for routing URLs to the appropriate view functions
-"""
+from src import db
+from src.api.models import Song
 
 
 def re_path(route, view, **kwargs):
+    """
+    Helper function used in src.api.routes.main_urls.py
+    for routing URLs to the appropriate view functions
+    """
     return view, route, kwargs
+
+
+def add_song(name, duration):
+    song = Song(
+        name=name,
+        duration=duration,
+    )
+    db.session.add(song)
+    db.session.commit()
+    return song
