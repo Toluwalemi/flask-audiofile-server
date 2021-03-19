2c8be69a563ef6153a90c1d344198c82973ce649
Toluwalemi <toluwalemisrael@gmail.com>
2021-03-19 02:28:33 +0100

    test: add new test

--- src/api/tests/test_views_audiobook.py
+++ src/api/tests/test_views_audiobook.py
@@ -28,4 +28,41 @@ class TestAudioBook(BaseTestCase):
             self.assertIn('zikora', data['message'])
             self.assertIn('success', data['status'])
 
-            print("\n=============================================================")
+        print("\n=============================================================")
+
+    def test_add_duplicate_audiobook(self):
+        """Ensure that an error is returned for a duplicate audiobook entry"""
+        with self.client:
+            self.client.post(
+                '/api/v1/audio/',
+                data=json.dumps({
+                    'audioFileType': 'audiobook',
+                    'audioFileMetadata': {
+                        'name': 'zikora',
+                        'duration': 2000,
+                        'author': 'Ngozi Adichie',
+                        'narrator': 'Adepero Oduye'
+                    }
+                }),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/api/v1/audio/',
+                data=json.dumps({
+                    'audioFileType': 'audiobook',
+                    'audioFileMetadata': {
+                        'name': 'zikora',
+                        'duration': 2000,
+                        'author': 'Ngozi Adichie',
+                        'narrator': 'Adepero Oduye'
+                    }
+                }),
+                content_type='application/json',
+            )
+
+            data = json.loads((response.data.decode()))
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('This audiobook already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+        print("\n=============================================================")
